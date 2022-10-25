202bec5509f3c73af2337978cb5f0b3b659f2b98
chernanq88 <chernanq88@gmail.com>
2022-10-25 18:35:39 -0400

    All paths of tree

--- src/trees/TreeClass.java
+++ src/trees/TreeClass.java
@@ -1,5 +1,10 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class TreeClass {
 
     static class TreeNode{
@@ -9,33 +14,68 @@ public class TreeClass {
 
     }
 
+    /**
+     *                  1
+     *              2        3
+     *          4     5   6    7
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
+        TreeNode four=new TreeNode();
+        four.value=4;
 
-        TreeNode left=new TreeNode();
-        left.value=1;
+        TreeNode six=new TreeNode();
+        six.value=6;
 
-        TreeNode right=new TreeNode();
-        right.value=3;
+        TreeNode five=new TreeNode();
+        five.value=5;
+
+        TreeNode seven= new TreeNode();
+        seven.value=7;
 
 
-        TreeNode root=new TreeNode();
-        root.value=2;
-        root.left=left;
-        root.right=right;
+
+        TreeNode two=new TreeNode();
+        two.value=2;
+        two.left=four;
+        two.right=five;
+
+        TreeNode three=new TreeNode();
+        three.value=3;
+        three.left= six;
+        three.right=seven;
+
+
+        TreeNode one=new TreeNode();
+        one.value=1;
+        one.left=two;
+        one.right=three;
 
         System.out.println("Inorder");
-        traverseTreeInorder(root);
+        traverseTreeInorder(one);
 
         System.out.println("Preorder");
-        traverseTreePreorder(root);
+        traverseTreePreorder(one);
 
         System.out.println("Postorder");
-        traverseTreePostOrder(root);
+        traverseTreePostOrder(one);
+
+        System.out.println();
+        System.out.println();
+
+        printAllPaths(one, new ArrayList<>());
+        result.forEach(list->{
+            list.forEach(System.out::print);
+            System.out.println();
+        });
     }
 
 
-
+    /**
+        o(n)
+     */
     static void traverseTreeInorder(TreeNode root){
 
         if(root.left!=null)
@@ -48,6 +88,10 @@ public class TreeClass {
 
     }
 
+    /**
+     * o(n)
+     * @param root
+     */
     static void traverseTreePreorder(TreeNode root){
 
         System.out.println(root.value);
@@ -60,6 +104,9 @@ public class TreeClass {
 
     }
 
+    /**
+     o(n)
+     */
     static void traverseTreePostOrder(TreeNode root){
 
         if(root.left!=null)
@@ -71,5 +118,41 @@ public class TreeClass {
         System.out.println(root.value);
 
     }
+
+
+    /**
+     *
+     *  o (m +n)
+     *  s log(n)
+     *
+     *  1 2 4
+     *  1 2 5
+     *  1 3 6
+     *  1 3 7
+     *
+     *
+     */
+    static List<List<Integer>> result=new ArrayList<>();
+    static void printAllPaths(TreeNode node, List<Integer> list){
+
+        if (node==null)
+            return;
+
+        list.add(node.value);
+
+        if(node.left!=null)
+            printAllPaths(node.left, list);
+
+        if (node.right!=null)
+            printAllPaths(node.right, list);
+
+        if (node.right==null && node.left==null)
+            result.add(new ArrayList<>(list));
+
+        list.remove(list.size()-1);
+
+    }
+
+
 
 }
