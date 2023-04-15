649115ae8378c72c98165aea96023a9092298105
chernanq88 <chernanq88@gmail.com>
2023-04-15 01:25:38 -0500

    Sorting and collecting with java 8

---
+++ src/java8/SortingAndCollecting.java
@@ -0,0 +1,43 @@
+package java8;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SortingAndCollecting {
+
+    public static void main(String[] args) {
+
+        record Employee(String name, int age, double salary){};
+
+        List<Employee> employee = List.of(new Employee("Carlos",34, 5000),
+                                              new Employee("Ana",34, 25000),
+                                              new Employee("Liliana",56,10000));
+
+        employee.stream().sorted(Comparator.comparing(Employee::age,Comparator.reverseOrder())).forEach(System.out::println);
+
+        System.out.println("""
+                """);
+
+
+        employee.stream().sorted(Comparator.comparing(Employee::age)).forEach(System.out::println);
+
+        System.out.println("""
+                """);
+
+        employee.stream().dropWhile(employee1 -> employee1.age==34).forEach(System.out::println);
+
+        System.out.println("""
+                """);
+
+        Map<Integer,List<Employee>>  map= employee.stream().collect(Collectors.groupingBy(Employee::age));
+
+        map.entrySet().stream().forEach(integerListEntry -> {
+
+            System.out.println(integerListEntry.getKey());
+            integerListEntry.getValue().forEach(i-> System.out.println(" " + i));
+        });
+    }
+
+}
